4ed555a572a212c0505c729cafaac87b3fdd7e2e
air <kobychev00@bk.ru>
2023-04-25 18:21:36 +0300

    1

---
+++ src/main/java/com/example/exeptions/ExeptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.exeptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExeptionsApplication.class, args);
+    }
+
+}
